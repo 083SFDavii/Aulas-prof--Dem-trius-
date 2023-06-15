071d34f9cead7badd57a08417660a2ae516cdacb
José Davi <127751617+083davii@users.noreply.github.com>
2023-06-15 11:56:37 -0300

    Add files via upload

---
+++ projetoA3/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" type = "text/css" href = "estilo.css">
+    <title>Document</title>
+</head>
+<body>
+    <CENTER>
+        <div class="Login">
+            <div class="cor"
+        <h1>Login</h1>
+<form id="Login" action="logado.php"
+method="POST">
+login: <input type="text" name="login" required><br> 
+senha: <input type="password" name="senha"required><br>
+        <input type="submit" name="entrar" value="entrar">
+
+    </center>
+
+
+    </form>
+</div>
+</body>
+</html> 
